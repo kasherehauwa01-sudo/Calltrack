4bbc7920bd35a7f923f1772d247b87703f608f5b
kasherehauwa01-sudo <kasherehauwa01@gmail.com>
2026-05-05 15:34:17 +0300

    Load contact history from remote webhook with phone filter

--- app/src/main/java/com/example/calltrack/data/remote/WebhookApi.kt
+++ app/src/main/java/com/example/calltrack/data/remote/WebhookApi.kt
@@ -1,10 +1,14 @@
 package com.example.calltrack.data.remote
 
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Url
 
 interface WebhookApi {
     @POST
     suspend fun sendCall(@Url url: String, @Body request: WebhookRequest)
+
+    @GET
+    suspend fun loadHistory(@Url url: String): List<CallHistoryItem>
 }
