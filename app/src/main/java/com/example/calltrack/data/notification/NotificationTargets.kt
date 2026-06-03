90c7d0403e5982957bf5198f44f32e74ad350262
kasherehauwa01-sudo <kasherehauwa01@gmail.com>
2026-06-03 17:34:48 +0300

    Fix update dialog listener types

---
+++ app/src/main/java/com/example/calltrack/data/notification/NotificationTargets.kt
@@ -0,0 +1,9 @@
+package com.example.calltrack.data.notification
+
+object NotificationTargets {
+    const val CALL_HISTORY = "CALL_HISTORY"
+    const val CALL_DETAIL = "CALL_DETAIL"
+    const val REMINDER = "REMINDER"
+    const val PERSONAL_CONTACT = "PERSONAL_CONTACT"
+    const val CONTACT_CARD = "CONTACT_CARD"
+}
